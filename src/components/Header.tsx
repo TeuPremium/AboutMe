4bc354bfcd2e8538a34af15954c826ab77a77273
vini <vinicius.brandaot@gmail.com>
2023-10-26 13:11:30 -0300

    initiated repositorie

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export default function Header() {
+  return (
+    <Container>
+      <div></div>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  height: 45px;
+  background-color: brown;
+  width: 100vw;
+  position: fixed;
+  top: 0;
+`;
