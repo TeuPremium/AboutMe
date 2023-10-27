f1eb5c2093afab054b8b213b38af310d2da9d5d3
vini <vinicius.brandaot@gmail.com>
2023-10-26 23:55:55 -0300

    fixed header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 export default function Header() {
   return (
     <Container>
-      <Link>
+      <Link to={"/"}>
         <div>TeuPremium</div>
       </Link>
     </Container>
